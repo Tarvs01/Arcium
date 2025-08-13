b631ad3318cb40bd4ae85f93a15477d8268afdce
Terver Torvenda <tervenda18@gmail.com>
2025-08-13 16:06:57 +0100

    corrected the svg error

--- src/Animation.tsx
+++ src/Animation.tsx
@@ -1,8 +1,15 @@
-import React from 'react'
+import AI from "./svg animations/applications/AI"
+import AIChip from "./svg animations/applications/AIChip"
+import Assessment from "./svg animations/applications/Assessment"
+import Bank from "./svg animations/applications/Bank"
+import PaymentCard from "./svg animations/applications/PaymentCard"
+import Sthetoscope from "./svg animations/applications/Sthetoscope"
 
 function Animation() {
   return (
-    <div className='animation-cont'>Animation</div>
+    <div className='animation-cont'>
+        {/* <AI /> */}
+    </div>
   )
 }
 
